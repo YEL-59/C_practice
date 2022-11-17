fb82000b5a0d1f3b8d08663f82f0e2c5a84720aa
Md. Tofayel Islam <69710505+YEL-59@users.noreply.github.com>
2022-11-17 17:33:40 +0600

    Update first.c

--- first.c
+++ first.c
@@ -474,3 +474,35 @@ int main()
 
     return 0;
 }
+
+
+// how to find the given array is sorted or not!
+#include <stdio.h>
+int main()
+{
+  int n, s[1000], a = 1, d = 1, i;
+
+  scanf("%d", &n);
+
+  for (i = 0; i < n; i++)
+    scanf("%d", &s[i]);
+
+  i = 0;
+
+  while ((a == 1 || d == 1) && i < n - 1) {
+    if (s[i] < s[i+1])
+      d = 0;
+    else if (s[i] > s[i+1])
+      a = 0;
+    i++;
+  }
+
+  if (a == 1)
+    printf("ascending order.\n");
+  else if (d == 1)
+    printf(" descending order.\n");
+  else
+    printf("The array is not sorted.\n");
+
+  return 0;
+}
